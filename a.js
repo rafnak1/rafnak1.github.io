bc6387bb6fc120d2f859d62fdc4094c4160110b8
Rafael Nakata <rafaelk.nakata@gmail.com>
2023-02-18 02:47:46 -0300

    actual fix

--- a.js
+++ a.js
@@ -2,6 +2,6 @@ count = () => {
     input = document.getElementById("input");
     c = input.value.length;
     result = document.getElementById("result");
-    // result.firstChild.remove()
+    result.firstChild.remove()
     result.appendChild(document.createTextNode(c));
 }
