64b7d2ba076fa0b54d865e1a78b37d73bad39582
Rafael Nakata <rafaelk.nakata@gmail.com>
2023-02-18 15:36:22 -0300

    refactor

--- a.js
+++ a.js
@@ -1,7 +1,10 @@
 count = () => {
     input = document.getElementById("input");
-    c = input.value.length;
-    result = document.getElementById("result");
-    result.firstChild.remove()
-    result.appendChild(document.createTextNode(c));
+    setResult(input.value.length);
+}
+
+setResult = (result) => {
+    resultElement = document.getElementById("result");
+    resultElement.firstChild.remove()
+    resultElement.appendChild(document.createTextNode(result));
 }
